0f9149c9efa5511b7ba73008127f52ddc30b3d8e
Pavlo <doesntexist514@gmail.com>
2022-06-06 09:54:36 +0300

    Commit

--- main.py
+++ main.py
@@ -63,9 +63,5 @@ def results():
     return render_template('results.html', queries=queries, doc_list=doc_list, qr=query[0])
 
 
-def getApp():
-    return app
-
-
 if __name__ == '__main__':
     app.run()
